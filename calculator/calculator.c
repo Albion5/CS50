9171ac49499853ef304931c4c43b607465b510fa
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-06 15:56:30 +0000

    Sun, Mar 6, 2022, 3:56 PM +00:00

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -3,10 +3,14 @@
 
 int main(void)
 {
+    // Promt user for x and y
     int x = get_int("x: ");
     int y = get_int("y: ");
-    // 
+
+    // Perform addition and assign the result to a sum variable
     int sum = x + y;
     printf("Equals: %i\n", sum);
+
+    // Perform addition and then print the result
     printf("%i\n", x + y);
 }

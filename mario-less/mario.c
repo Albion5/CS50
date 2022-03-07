cee3f21c4d1092e938de485d257f4e49e02769ea
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-07 19:11:10 +0000

    Mon, Mar 7, 2022, 7:11 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while ((height < 1) || (height > 8));
+
 }

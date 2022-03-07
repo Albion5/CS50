733ed04c8b87541cfbea422e29f5790a0ed99577
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-07 18:50:45 +0000

    Mon, Mar 7, 2022, 6:50 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-    // Ask user 
+    // Ask the user to enter their name
     string name = get_string("Enter your name here: ");
+    // Print out the greeting
     printf("Hello, %s!\n", name);
 }

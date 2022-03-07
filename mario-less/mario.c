3c63c233589b6a53b5bed13f1b5c9479bb62b8c2
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-07 20:00:28 +0000

    Mon, Mar 7, 2022, 8:00 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -13,22 +13,24 @@ int main(void)
         height = get_int("Height: ");
     }
     while ((height < 1) || (height > 8));
-
+    // Call the funtion to build the pyramid of a certain height
     build(height);
 
 }
 
+// Builds the pyramid of a certain height
 void build(int size)
 {
+    // For each row
     for (int i = 1; i < size + 1; i++)
     {
         int dots = size - i;
-
+        // Dots for each column
         for (int j = 0; j < dots; j++)
         {
             printf(" ");
         }
-
+        // Hashes for each column
         for (int k = 0; k < i; k++)
         {
             printf("#");

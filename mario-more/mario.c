9b52f4a55d03094539d35ba75097fe4ebe8b8e07
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-07 20:23:13 +0000

    Mon, Mar 7, 2022, 8:23 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,46 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void build(int size);
+
+int main(void)
 {
-    
+    // Initialize the height variable
+    int height;
+    // Promt the user to enter the height between 1 and 8
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while ((height < 1) || (height > 8));
+    // Call the funtion to build the pyramid of a certain height
+    build(height);
+
+}
+
+// Builds the pyramid of a certain height
+void build(int size)
+{
+    // For each row
+    for (int i = 1; i < size + 1; i++)
+    {
+        // Calculate the number of spaces on each row
+        int spaces = size - i;
+
+        // Spaces for each column
+        for (int j = 0; j < spaces; j++)
+        {
+            // Print spaces
+            printf(" ");
+        }
+        // The number of hashes for each column equals i
+        for (int k = 0; k < i; k++)
+        {
+            // Print hashes
+            printf("#");
+        }
+        // Go to the next line
+        printf("\n");
+    }
+
 }

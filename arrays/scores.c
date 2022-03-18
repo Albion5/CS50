4d8c6b14eff2cfc8d8e782a7ce523d8f64195079
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-18 18:34:23 +0000

    Fri, Mar 18, 2022, 6:34 PM +00:00

--- arrays/scores.c
+++ arrays/scores.c
@@ -6,11 +6,11 @@ int main(void)
     int n = get_int("How many scores? ");
 
     int scores[n];
-
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         scores[i] = get_int("Score: ");
+        sum += scores[i];
     }
-
-    printf("Average: %f/n", (scores[]));
+    printf("Average: %f\n", sum / n);
 }

2f03a77ba807f478c232706b285b08e0b5f06a6d
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-18 18:04:41 +0000

    Fri, Mar 18, 2022, 6:04 PM +00:00

--- arrays/upper.c
+++ arrays/upper.c
@@ -8,7 +8,9 @@ int main(void)
     printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 97 && s[i] <= 122)
+        // Capital letter = lower + 32
+        // 'a' = 97, 'z' = 122; 'A' = 'a' + 32, 'Z' = 'z' + 32
+        if (s[i] >= 'a' && s[i] <= 'z')
         {
             printf("%c", s[i] - 32);
         }

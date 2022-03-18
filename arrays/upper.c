390d0c3bede0fec4a6ade5a45ecc579554fa466b
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-18 18:01:39 +0000

    Fri, Mar 18, 2022, 6:01 PM +00:00

--- arrays/upper.c
+++ arrays/upper.c
@@ -8,7 +8,7 @@ int main(void)
     printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
+        if (s[i] >= 97 && s[i] <= 122)
         {
             printf("%c", s[i] - 32);
         }

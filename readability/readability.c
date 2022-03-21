8bc35d98d8851c8b1c7a403db5f42137cc3eb8a1
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-21 11:54:59 +0000

    Mon, Mar 21, 2022, 11:54 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,9 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text);
+
 int main(void)
 {
-    string text = get_string("Text: ");
-    
+    string user_text = get_string("Text: ");
+    int letters = count_letters(user_text);
 
+}
+
+int count_letters(string text)
+{
+    int sum = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if text[i] 
+
+    }
 }

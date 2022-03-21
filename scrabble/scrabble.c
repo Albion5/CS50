6815b781f079775f9662d45f8c62706745658d00
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-21 16:12:54 +0000

    Mon, Mar 21, 2022, 4:12 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,7 +2,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <ctype.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -27,6 +27,13 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
-    if
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        char current = word[i];
+        if isalpha(current)
+        {
+
+        }
+    }
 
 }

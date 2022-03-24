68aa0ea415c3306b8fede3ff35a1cfbce9c5dfe9
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-03-24 21:27:13 +0000

    Thu, Mar 24, 2022, 9:27 PM +00:00

---
+++ rotate.c
@@ -0,0 +1,14 @@
+    //if (islower(letter))
+    //{
+        //if (conv_letter > 'z')
+        //{
+            //conv_letter -= 26;
+        //}
+    //}
+    //else
+    //{
+        //if (conv_letter > 'Z')
+        //{
+            //conv_letter -= 26;
+        //}
+    //}

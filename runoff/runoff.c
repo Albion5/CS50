c3e1c4bbdfdc9e834b142cf8374eccf7fbf2d5e4
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-04-09 17:07:57 +0000

    Sat, Apr 9, 2022, 5:07 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -136,7 +136,7 @@ bool vote(int voter, int rank, string name)
         if (strcmp(name, cand_name) == 0)
         {
             printf("Valid vote.\n");
-            preferences[i][rank] = ;
+            preferences[MAX_VOTERS][i];
             return true;
         }
     }

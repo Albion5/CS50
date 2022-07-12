217a42d2370f7cb6665a81f04336e7e85d738085
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-07-12 19:05:53 +0000

    Tue, Jul 12, 2022, 7:05 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,11 +3,12 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int rgbav = 0;
     for (int i; i = 0; i < height)
     {
         for (int j; j = 0; j < width)
         {
-            
+            rgav =
         }
     }
     return;

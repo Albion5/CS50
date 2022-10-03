78ce7b1c44a5b0da19df1a42adc6708c1eb1e23a
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-03 09:10:10 +0000

    Mon, Oct 3, 2022, 9:10 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -30,15 +30,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             originalGreen = image[i][j].rgbtGreen;
             originalBlue = image[i][j].rgbtBlue;
             sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
-            if (sepiaRed > 255) {
+            if (sepiaRed > 255)
+            {
                 sepiaRed = 255;
             }
             sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
-            if (sepiaGreen > 255) {
+            if (sepiaGreen > 255)
+            {
                 sepiaGreen = 255;
             }
             sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
-            if (sepiaBlue > 255) {
+            if (sepiaBlue > 255)
+            {
                 sepiaBlue = 255;
             }
             image[i][j].rgbtRed = sepiaRed;
@@ -94,11 +97,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             avRed = 0;
             avGreen = 0;
             avBlue = 0;
-            for (int y = -1; y <= 1; y++) {
+            for (int y = -1; y <= 1; y++)
+            {
                 int next_y = i + y;
-                for (int x = -1; x <= 1; x++) {
+                for (int x = -1; x <= 1; x++)
+                {
                     int next_x = j + x;
-                    if (next_x < width && next_x >= 0 && next_y < height && next_y >= 0) {
+                    if (next_x < width && next_x >= 0 && next_y < height && next_y >= 0)
+                    {
                         neighbours += 1;
                         avRed += image_copy[next_y][next_x].rgbtRed;
                         avGreen += image_copy[next_y][next_x].rgbtGreen;

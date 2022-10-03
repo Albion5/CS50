92d55b3fd02bff830b50265eda8e03b0d77fdc26
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-03 08:09:54 +0000

    Mon, Oct 3, 2022, 8:09 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -21,15 +21,19 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    int rgbav = 0;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            int originalRed, originalGreen, originalBlue, sepiaRed, sepiaGreen, sepiaBlue;
+            originalRed = image[i][j].rgbtRed;
+            originalGreen = image[i][j].rgbtGreen;
+            originalBlue = image[i][j].rgbtBlue;
+            sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+            sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+            sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+
             rgbav = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
-            image[i][j].rgbtRed = rgbav;
-            image[i][j].rgbtGreen = rgbav;
-            image[i][j].rgbtBlue = rgbav;
         }
     }
     return;

5726e8043d003d1148f77c1bbf6cb69e21ee4b03
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-03 08:12:53 +0000

    Mon, Oct 3, 2022, 8:12 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -29,11 +29,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             originalRed = image[i][j].rgbtRed;
             originalGreen = image[i][j].rgbtGreen;
             originalBlue = image[i][j].rgbtBlue;
-            sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
-            sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
-            sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
-
-            rgbav = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue) % 255;
+            sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue) % 255;
+            sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue) % 255;
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;

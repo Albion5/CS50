1a9387879a85df65a20d5fbf2b0e036dc283bb78
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-08 14:58:41 +0000

    Sat, Oct 8, 2022, 2:58 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -21,6 +21,25 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    int new_red, new_green, new_blue;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            new_red = get_neighbours(x, y, image);
+            new_green = get_neighbours(x, y, image);
+            new_blue = get_neighbours(x, y, image);
+            image[y][x].rgbRed = new_red;
+            image[y][x].rgbGreen = new_green;
+            image[y][x].rgbBlue = new_blue;
+        }
+    }
+
     return;
+}
+
+int get_neighbors(int cur_y, int cur_x, int **image) {
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
 }

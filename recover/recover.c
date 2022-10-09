89e08dd4fba4db75783d6997353e3bbbecd1b777
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-09 04:21:35 +0000

    Sun, Oct 9, 2022, 4:21 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,22 +24,28 @@ int main(int argc, char *argv[])
     int n = 0;
     char file_name[8];
     FILE *jpeg1 = NULL;
+    // Repeat untill end of the file
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
+        // If found JPEG
         if (check_JPEG(buffer))
         {
+            // Create a file name with its number
             sprintf(file_name, "%03i.jpg", n);
+            // If not first JPEG close previosly opened JPEG
             if (n > 0)
             {
                 fclose(jpeg1);
 
             }
+            // Open new JPEG and write to file
             jpeg1 = fopen(file_name, "w");
             fwrite(buffer, BLOCK_SIZE, 1, jpeg1);
             n++;
         }
         else
         {
+            // If not start of JPEG but there is an open file, continue writing to it
             if (n > 0)
             {
                 fwrite(buffer, BLOCK_SIZE, 1, jpeg1);
@@ -54,10 +60,11 @@ int main(int argc, char *argv[])
     fclose(raw_file);
 }
 
-// Check if the
+// Check if start of a new JPEG
 int check_JPEG(BYTE buffer[BLOCK_SIZE])
 {
     int result = 0;
+    // Check first 4 bytes of block of size 512 bytes (0xff 0xd8 0xff 0xe*)
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
     {
         if ((buffer[3] & 0xf0) == 0xe0)

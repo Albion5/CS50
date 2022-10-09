0c4d9b9f996200b50050da342ce3961f00c11fb0
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-09 16:12:15 +0000

    Sun, Oct 9, 2022, 4:12 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -60,24 +60,30 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-
+        child->parents[0] = NULL;
+        child->parents[0] = NULL;
         // TODO: Randomly assign alleles
-
+        child->alleles[0] = random_allele();
+        child->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return child;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
-
+    free_family(child->parents[0]);
+    free_family(child->parents[1]);
     // TODO: Free child
-
+    free(p);
 }
 
 // Print each family member and their alleles.

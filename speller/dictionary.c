8aeacc83f8c7435e069a1781b1dc547f9d36f5dc
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-15 18:07:34 +0000

    Sat, Oct 15, 2022, 6:07 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -69,7 +69,7 @@ bool load(const char *dictionary)
             strcpy(new->word, string);
         }
     }
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded

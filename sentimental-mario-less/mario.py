f00da1a11bac9f36c7c97e4d082c8ea27f82354b
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-23 18:15:03 +0000

    Sun, Oct 23, 2022, 6:15 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,16 @@
 # TODO
+from cs50 import get_int
+
+def check_height(number):
+    return number >= 1 and number <= 8
+
+def build(height):
+    n_spaces = height - 1
+    for i in range(height):
+        print("#")
+
+height = 0
+while not check_height(height):
+     height = get_int("Height: ")
+
+build(height)

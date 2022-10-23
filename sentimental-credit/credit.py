4a77f74b93091082ce19efda4f908203e029a4c7
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-23 19:43:02 +0000

    Sun, Oct 23, 2022, 7:43 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -67,7 +67,6 @@ def get_card(number, length):
 
     # Delete all of the digits except the first two
     start_digits = number // denominator
-    print(start_digits)
     # Check if it's MASTERCARD
     if (start_digits > 50 and start_digits <= 55):
         return "MASTERCARD"

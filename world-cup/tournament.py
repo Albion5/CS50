77f124916674f9e4987996a1b8684b22a12cfc1b
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-23 17:38:31 +0000

    Sun, Oct 23, 2022, 5:38 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -25,13 +25,16 @@ def main():
             teams.append(team)
 
     counts = {}
-    for team_dict in teams:
-        name = team_dict["team"]
-        counts[name] = 0
+    # for team_dict in teams:
+    #     name = team_dict["team"]
+    #     counts[name] = 0
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N):
         winner = simulate_tournament(teams)
-        counts[winner] += 1
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):

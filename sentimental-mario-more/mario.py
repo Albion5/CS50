a4ae4d0f9e72f29a8ab6b566cd58f98206c97469
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-23 18:42:18 +0000

    Sun, Oct 23, 2022, 6:42 PM +00:00

---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,23 @@
+# TODO
+from cs50 import get_int
+
+
+# Check if height is valid number between 1 and 8
+def check_height(number):
+    return number >= 1 and number <= 8
+
+
+# Print a pyramid of a given height
+def build(height):
+    for i in range(1, height + 1):
+        n_spaces = height - i
+        print((n_spaces * " ") + ("#" * i) + "  " + ("#" * i))
+
+
+height = 0
+# Keep prompting user untill he enters a valid height
+while not check_height(height):
+    height = get_int("Height: ")
+
+# Print a pyramid of a given height
+build(height)

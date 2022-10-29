284619e76f1a35076e8b1f07aaa45fe59feb0ea9
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-29 16:40:56 +0000

    Sat, Oct 29, 2022, 4:40 PM +00:00

--- test/s21_cat.c
+++ test/s21_cat.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#define buf_size 4096
 
-void s21_cat() {
+void s21_cat(int fd) {
+    char buffer[buf_size];
+    int bytes_read = read(fd, buffer, buf_size);
+    while 
 
 }
 

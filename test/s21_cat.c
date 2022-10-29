ed3826636125376389fcba3ade0db3ec3c216604
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-29 17:12:46 +0000

    Sat, Oct 29, 2022, 5:12 PM +00:00

--- test/s21_cat.c
+++ test/s21_cat.c
@@ -4,6 +4,16 @@
 #include <fcntl.h>
 #define buf_size 4096
 
+typedef struct Flags {
+    int number_all;
+    int number_non_empty;
+    int show_tab;
+    int show_end;
+    int show_unprintable;
+    
+
+} Flags;
+
 void s21_cat(int fd) {
     char buffer[buf_size];
     int bytes_read = read(fd, buffer, buf_size);

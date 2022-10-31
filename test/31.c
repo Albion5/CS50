6f5800ae6686859452fdff181110f657f107d59f
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-31 07:44:36 +0000

    Mon, Oct 31, 2022, 7:44 AM +00:00

--- test/31.c
+++ test/31.c
@@ -96,13 +96,13 @@ void print_with_flags(char *buffer, int len, Flags *flags, int *empty_repeat) {
                     printf("^l");
                 }  else if (flags->show_unprintable) {
                     if (cur_char >= 0 && cur_char <= 32) {
-                        printf("^%c", cur_char + 64);
+                        printf("^%d", cur_char + 64);
                     } else if (cur_char < 127) {
 
                     } else if (cur_char == 127) {
                         printf("^?");
                     } else if (cur_char < 128 + 64) {
-                        printf("M-^%c", cur_char - 64);
+                        printf("M-^%d", cur_char - 64);
                     }
                 } else {
                     printf("%c", cur_char);

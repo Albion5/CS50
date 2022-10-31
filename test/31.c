f8e82033cf76681c0cfb296addf98180f9a9375f
Albion5 <95123505+Albion5@users.noreply.github.com>
2022-10-31 09:14:04 +0000

    Mon, Oct 31, 2022, 9:14 AM +00:00

--- test/31.c
+++ test/31.c
@@ -186,25 +186,25 @@ int get_number_of_flags(Flags *flags) {
 int read_line(unsigned char *buffer, FILE *source, int *len, int *position) {
     fseek(source , *position , SEEK_SET );
     int read = 1;
-    // *len = 0;
-    // int i = -1;
-    // unsigned char c = fgetc(source);
+    *len = 1;
+    int i = 0;
+    unsigned char c = fgetc(source);
     while (c != EOF && c != '\n') {
         // Remove trailing newline
         // buffer[strcspn(buffer, "\n")] = 0;
-        buffer[++i] = c;
+        buffer[i] = c;
         (*len)++;
         c = fgetc(source);
         // printf("%c", c + 64);
     }
-    // buffer[++i] = c;
-    // (*len)++;
-    // // buffer[++i] = '\0';
+    buffer[i++] = c;
+    (*len)++;
+    buffer[i++] = '\0';
     // // (*len)++;
     if (c == EOF) {
         read = 0;
     }
-    // *position += *len;
+    *position += *len;
     return read;
 }
 
